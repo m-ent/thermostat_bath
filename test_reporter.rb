01293211e651ba7a5d6a470337ebbc4f3adb9a96
Mamiya ENT Clinic <mamiya.ent@gmail.com>
2019-09-16 02:27:49 +0900

    little addition to test_reporter.rb

--- test_reporter.rb
+++ test_reporter.rb
@@ -78,32 +78,51 @@ describe "Thermobath reporter" do
       end
     end
 
-    describe "古い stat file (タイムスタンプが30分以上前)がある場合" do
+    describe "古い stat file がある場合" do
       before do
         @stat_file = "/tmp/thermobath_status.dat"
         @temp = 30.0 + rand(100) / 10.0
         File.open(@stat_file, 'w') do |f|
           f.puts "#{@temp}, 36.0" # temperature, target temp
         end
-        File.utime(Time.now - 31 * 60, Time.now - 31 * 60, @stat_file)
-        get '/get_status'
-        @response = last_response
       end
 
-      it "response 200 を返すこと" do
-        @response.ok?.must_equal true
+      describe "タイムスタンプが30分以上前の場合" do
+        before do
+          File.utime(Time.now - 31 * 60, Time.now - 31 * 60, @stat_file)
+          get '/get_status'
+          @response = last_response
+        end
+
+        it "response 200 を返すこと" do
+          @response.ok?.must_equal true
+        end
+
+        it "現在の温度として json で -274.0 を返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('temp').must_equal true
+          res['temp'].to_f.must_equal -274.0
+        end
+
+        it "目標温度として json で -274.0 を返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('target').must_equal true
+          res['target'].to_f.must_equal -274.0
+        end
       end
 
-     it "現在の温度として json で -274.0 を返すこと" do
-        res = JSON.parse(@response.body)
-        res.has_key?('temp').must_equal true
-        res['temp'].to_f.must_equal -274.0
-      end
+      describe "タイムスタンプが30分以上前ではない場合" do
+        before do
+          File.utime(Time.now - 29 * 60, Time.now - 29 * 60, @stat_file)
+          get '/get_status'
+          @response = last_response
+        end
 
-      it "目標温度として json で -274.0 を返すこと" do
-        res = JSON.parse(@response.body)
-        res.has_key?('target').must_equal true
-        res['target'].to_f.must_equal -274.0
+        it "現在の温度を json で返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('temp').must_equal true
+          res['temp'].to_f.must_equal @temp
+        end
       end
 
       after do
