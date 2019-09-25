2386aaa20b01e9c648f39dcb29cc80e2753633b1
Mamiya ENT Clinic <mamiya.ent@gmail.com>
2019-09-26 00:52:07 +0900

    wait_and_rn.rb: replace candidate of main.rb

---
+++ test_wait_and_run.rb
@@ -0,0 +1,162 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require 'rack/test'
+require './wait_and_run'
+
+include Rack::Test::Methods
+
+def app
+  Sinatra::Application
+end
+
+describe "Thermobath reporter" do
+  describe "GET /" do
+    it "response 200 を返すこと" do
+      get '/'
+      last_response.ok?.must_equal true
+    end
+  end
+
+  describe "GET /get_status" do
+    describe "正しい stat file がある場合" do
+      before do
+        @stat_file = "/tmp/thermobath_status.dat"
+        @temp = 30.0 + rand(100) / 10.0
+        File.open(@stat_file, 'w') do |f|
+          f.puts "---, #{@temp}, 36.0" # (go/idle), temperature, target temp
+        end
+        get '/get_status'
+        @response = last_response
+      end
+
+      it "response 200 を返すこと" do
+        @response.ok?.must_equal true
+      end
+
+       it "現在の温度を json で返すこと" do
+        res = JSON.parse(@response.body)
+        res.has_key?('temp').must_equal true
+        res['temp'].to_f.must_equal @temp
+      end
+
+      it "目標温度を json で返すこと" do
+        res = JSON.parse(@response.body)
+        res.has_key?('target').must_equal true
+        res['target'].to_f.must_equal 36.0
+      end
+
+      after do
+        File.delete(@stat_file) if File.exists?(@stat_file)
+      end
+    end
+
+    describe "stat file がない場合" do
+      before do
+        @stat_file = "/tmp/thermobath_status.dat"
+        File.delete(@stat_file) if File.exists?(@stat_file)
+        get '/get_status'
+        @response = last_response
+      end
+
+      it "response 200 を返すこと" do
+        @response.ok?.must_equal true
+      end
+
+     it "現在の温度として json で -274.0 を返すこと" do
+        res = JSON.parse(@response.body)
+        res.has_key?('temp').must_equal true
+        res['temp'].to_f.must_equal -274.0
+      end
+
+      it "目標温度として json で -274.0 を返すこと" do
+        res = JSON.parse(@response.body)
+        res.has_key?('target').must_equal true
+        res['target'].to_f.must_equal -274.0
+      end
+    end
+
+    describe "古い stat file がある場合" do
+      before do
+        @stat_file = "/tmp/thermobath_status.dat"
+        @temp = 30.0 + rand(100) / 10.0
+        File.open(@stat_file, 'w') do |f|
+          f.puts "---, #{@temp}, 36.0" # (go/idle), temperature, target temp
+        end
+      end
+
+      describe "タイムスタンプが30分以上前の場合" do
+        before do
+          File.utime(Time.now - 31 * 60, Time.now - 31 * 60, @stat_file)
+          get '/get_status'
+          @response = last_response
+        end
+
+        it "response 200 を返すこと" do
+          @response.ok?.must_equal true
+        end
+
+        it "現在の温度として json で -274.0 を返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('temp').must_equal true
+          res['temp'].to_f.must_equal -274.0
+        end
+
+        it "目標温度として json で -274.0 を返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('target').must_equal true
+          res['target'].to_f.must_equal -274.0
+        end
+      end
+
+      describe "タイムスタンプが30分以上前ではない場合" do
+        before do
+          File.utime(Time.now - 29 * 60, Time.now - 29 * 60, @stat_file)
+          get '/get_status'
+          @response = last_response
+        end
+
+        it "現在の温度を json で返すこと" do
+          res = JSON.parse(@response.body)
+          res.has_key?('temp').must_equal true
+          res['temp'].to_f.must_equal @temp
+        end
+      end
+
+      after do
+        File.delete(@stat_file) if File.exists?(@stat_file)
+      end
+    end
+  end
+
+  describe "PUT /set_direction" do
+    before do
+      @direction_file = '/tmp/direction.dat'
+    end
+
+    it "direction を stop から run に変更できる" do
+      File.open(@direction_file, 'w') do |f|
+        f.puts "stop"
+      end
+      put '/set_direction/run?target=38.0'
+      last_response.ok?.must_equal true # response 200 を>    返すこと
+      File.open(@direction_file) do |f|
+        f.gets.chomp.must_match /^38.0/
+      end
+    end
+
+    it "direction を run から stop に変更できる" do
+      File.open(@direction_file, 'w') do |f|
+        f.puts "38.0"
+      end
+      put '/set_direction/stop'
+      last_response.ok?.must_equal true # response 200 を>    返すこと
+      File.open(@direction_file) do |f|
+        f.gets.chomp.must_match /^stop/
+      end
+    end
+  end
+
+end
---
+++ wait_and_run.rb
@@ -0,0 +1,166 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+require './controller'
+set :bind, '0.0.0.0'
+
+target = ARGV.shift
+case target
+when /\d+\.?\d*/
+else
+  target = 38.0
+end
+temp = target.to_f
+coefficient = 0.95
+resting_time = 30 #[sec]
+
+def rest(t)
+  puts "rest for #{t} second."
+  sleep t
+end
+
+th = Thread.new do # 別スレッドで PID 制御を実行
+  c = Thermo_controller.new(temp, 10, 0.098, 0.000567, 4.234)
+  c.on_fly(:on)
+  t0 = c.get_temp
+  t0 = 0.1 if t0 <= 0
+  puts "start: #{t0} to #{temp} [degree]."
+  puts "----------------"
+  t = t0
+  i = 0
+  while t < temp * coefficient do
+    i += 1
+    t /= coefficient
+  end 
+
+  puts "step = #{i}"
+  skip_last_step = false
+
+  i.times do |ii|
+    tt = temp * coefficient ** (i - ii)
+    puts "go to #{tt} degree"
+    c = Thermo_controller.new(tt, 10, 0.098, 0.000567, 4.234)
+    c.log(:on)
+    c.on_fly(:on)
+    if c.get_temp / tt > 0.95
+      puts "skiping"
+      skip_last_step = true
+      next
+    end
+    skip_last_step = false
+    c.run_until_temp_become(tt)
+    rest(resting_time)
+  end
+
+  rest(resting_time) if skip_last_step
+  puts "now, start the maintainance cycle."
+  c = Thermo_controller.new(temp, 10, 0.098, 0.000567, 4.234)
+  c.log(:on)
+  c.on_fly(:on)
+  c.start
+end
+
+# ここから sinatra の設定: 温度報告用
+
+get '/' do
+  erb :index
+end
+
+get '/get_status' do
+  @stat_file = "/tmp/thermobath_status.dat"
+  @temp, @target = '-274.0', '-274.0' # エラー値
+  if File.exists?(@stat_file)
+    if  (Time.now - File::Stat.new(@stat_file).mtime) < 30 * 60 
+      File.open(@stat_file) do |f|
+        l = f.gets
+        if m = /(.+),\s*(.+),\s*(.+)/.match(l)
+          @temp, @target = m[2], m[3]
+        end
+      end
+    end
+  end
+  {temp: @temp, target: @target}.to_json
+end
+
+put '/set_direction/:state' do
+  #  params = {"target"=>"38.0", "state"=>"run"}
+  #  params = {"state"=>"stop"}
+  @direction_file = '/tmp/direction.dat'
+  case params['state']
+  when 'stop'
+    direction = "stop"
+  when 'run'
+    direction = params['target']
+  end
+  File.open(@direction_file, 'w') do |f|
+    f.puts(direction)
+  end
+end
+
+__END__
+
+@@index
+<head> <title>Thermostat bath status report</title> </head>
+<body>
+  <p>
+  [Console]</br>
+    <input id="run_btn" type="button" value="保温">
+    <input id="target_temp" type="text" size="4" value="38.0">℃    |  
+    <input id="stop_btn" type="button" value="停止">
+  <p>
+  [Status]</br>
+    現在温度: <span id="temp"> </span> ℃
+    (目標温度: <span id="target"> </span> ℃): @<span id="time"> </span>
+  </p>
+
+  <script type='text/javascript' src='jquery-3.3.1.min.js'></script>
+
+  <script type="text/javascript">
+    $(function(){
+      function startTimer(){
+        timer = setInterval(exec, 5000); // 5000ms毎に exec() を実行する
+      }                                  // その情報をtimer変数へ入れている。
+
+      startTimer(); //タイマー開始
+
+      function exec(){
+        $.ajax({
+          type: "GET",
+          url: "/get_status",
+          dataType: "json",
+          success: function(json) {
+            $('#temp').text(json.temp);
+            $('#target').text(json.target);
+          },
+          error: function() {
+            $('#temp').text('---');
+            $('#target').text('---');
+          }
+        });
+        $('#time').text((new Date()).getHours() + ":" + (new Date()).getMinutes() + ":" + (new Date()).getSeconds());
+      }
+    });
+
+    $("#run_btn").click(function(){
+      var target_val = $("#target_temp").val();
+      if (target_val.search(/^\d+\.?\d*$/) == -1){
+          target_val = "38.0"
+      }
+      $("#target_temp").val(target_val);
+      send_data = "target=" + target_val;
+      $.ajax({
+        url: '/set_direction/run',
+        type: 'PUT',
+        data: send_data
+      });
+    });
+
+    $("#stop_btn").click(function(){
+      $.ajax({
+        url: '/set_direction/stop',
+        type: 'PUT',
+      });
+    });
+
+  </script>
+</body>
